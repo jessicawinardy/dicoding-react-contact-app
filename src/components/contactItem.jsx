ca36494b51464092fe2611e7ccc6f4a5215bd444
jessicawinardy <jessicawinardy@gmail.com>
2023-11-09 21:20:31 +0800

    new delete button

--- src/components/contactItem.jsx
+++ src/components/contactItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactItemBody from "./contactItemBody";
 import ContactItemImage from "./contactItemImage";
+import DeleteButton from "./deleteButton";
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   );
 }
